9a570c2bc60c096e2a6be8257e03cf17a9875fbb
Huy Cao <huycao@Huys-MacBook-Pro.local>
2025-11-06 09:38:54 +0700

    Upgrade deploy

--- build-develop.sh
+++ build-develop.sh
@@ -3,6 +3,6 @@
 git fetch origin
 git checkout develop
 git pull --rebase origin develop
-docker buildx build --platform linux/amd64 -f Dockerfile -t odoo:latest .
-docker image tag odoo:latest 103.20.144.134:80/develop/odoo:latest
-docker push 103.20.144.134:80/develop/odoo:latest
+docker buildx build --platform linux/amd64 -f Dockerfile -t odoo:latest --progress=plain .
+docker image tag odoo:latest harbor.metaserv.vn/develop/odoo:latest
+docker push harbor.metaserv.vn/develop/odoo:latest
